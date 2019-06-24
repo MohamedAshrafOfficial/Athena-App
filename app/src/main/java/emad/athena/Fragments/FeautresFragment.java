22bd87ae243b6520ee484fcc937aa2ba49c4c526
emad <emad.hetata97@gmail.com>
2019-06-24 22:12:56 +0200

    update theme feedback

--- app/src/main/java/emad/athena/Fragments/FeautresFragment.java
+++ app/src/main/java/emad/athena/Fragments/FeautresFragment.java
@@ -108,7 +108,7 @@ public class FeautresFragment extends Fragment {
         Lphonecall.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               helper.phoneCall();
+
             }
         });
 
